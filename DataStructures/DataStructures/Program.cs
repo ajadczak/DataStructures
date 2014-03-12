df6c9312196362bb25c64d9023415de012dfdf56
ajadczak <alex.jadczak@gmail.com>
2014-03-12 08:20:45 -0400

    Started laying out a hash map that can be composed of different hash and bucket implementations

--- DataStructures/DataStructures/Program.cs
+++ DataStructures/DataStructures/Program.cs
@@ -10,6 +10,16 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
+            BasicHash<string> hash = new BasicHash<string>();
+            LinkedListChain<string> chain = new LinkedListChain<string>();
+            HashMap<string> objectMap = new HashMap<string>(hash, chain);
+            
+            string myData = "Some object data";
+            objectMap.Insert(myData);
+
+            var val = objectMap.Get(myData);
+
+            HashSet<string> hashSet = new HashSet<string>();
         }
     }
 }
